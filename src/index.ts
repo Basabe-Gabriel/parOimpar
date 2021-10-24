cdfa8ca65e41de9b119b629cc6325a775e04fd4e
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-10-24 09:39:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let num: number = 0;
+
+if (num == 0) {
+  console.log("El " + num + " no es par ni impar");
+} else if (num % 2 == 0) {
+  console.log("El " + num + " es par");
+} else {
+  console.log("El " + num + " es impar");
+}
